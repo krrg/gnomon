ddeedc63e7279c1a7536d27af7814a4c1a8023ee
krrg <krrgithub@gmail.com>
2014-11-19 16:12:15 -0700

    Added flask-assets and scss for custom styling.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,25 @@
 from flask import Flask
+from flask.ext import assets
+import os
+
 app = Flask(__name__)
+env = assets.Environment(app)
+
+# Tell flask-assets where to look for the sass/scss files.
+
+env.load_path = [
+    os.path.join(os.path.dirname(__file__), 'assets/scss')
+]
+
+env.register(
+    'css_base',
+    assets.Bundle(
+        'base.scss',
+        filters='scss',
+        output='_gen/css_base.css'
+    )
+)
+
 
 # Import all the views here
 from views import root
