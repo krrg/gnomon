e5a4f5e320322d6262d70a12daea62b7778a350e
curtis <cwig5945@gmail.com>
2014-11-20 19:59:59 -0700

    added timesheet prototype

--- app/views/root.py
+++ app/views/root.py
@@ -7,3 +7,8 @@ from flask import render_template
 @app.route('/index.html')
 def root_index():
     return render_template("root/index.html")
+
+@app.route('/timesheet')
+@app.route('/timesheet.html')
+def timesheet():
+    return render_template("root/timesheet.html")
